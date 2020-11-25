1b40864278178de735dd93e90d341039bed73bb0
Dan Portal <danportal89@gmail.com>
2020-11-25 16:35:43 +0100

    update navbar new and image overlays

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "react-bootstrap/Navbar";
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
@@ -11,7 +14,6 @@ import Customers from "./pages/customers";
 import Home from "./pages/home";
 import Winetanks from "./pages/winetanks";
 import Image from "react-bootstrap/Image";
-import Form from 'react-bootstrap/Form';
 import Media from 'react-bootstrap/Media';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Card from 'react-bootstrap/Card'
@@ -21,92 +23,75 @@ function App() {
 
 
   return (
-    <div 
-      style={{
-        backgroundColor: '#F1EDE2', 
+<div style={{ backgroundColor: '#EEECE6'}}>
 
 
-      }}
-      >
-
-    <div className="App"> 
-  <Navbar>
-  <Navbar.Brand href="#home">logo needs to go here </Navbar.Brand>
-  <Navbar.Toggle />
-  <Navbar.Collapse className="justify-content-end">
-    <Navbar.Text>
-      <a href="#login">request button goes here</a>
-    </Navbar.Text>
-  </Navbar.Collapse>
-</Navbar>
-</div>
+<Navbar style={{ backgroundColor: '#F1EDE2'}}>
+    <Navbar.Brand href="#home">LOGO HERE</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#winetanks">Wine Tanks</Nav.Link>
+      <Nav.Link href="#benefits">Benefits of Concrete</Nav.Link>
+      <Nav.Link href="#about">Our Story</Nav.Link>
+      <Nav.Link href="#contact">Contact Us</Nav.Link>
+    </Nav>
+    <Form inline>
+      <Button href="#contact" variant="outline-primary">Request A Quote</Button>
+    </Form>
+  </Navbar>
 
 {/* First Image 
 Note: Needs text on image  */}
-<div>
-<Container fluid >
-      <Image src= "Images/ImageAboveNewText.jpg" rounded
-      width= "100%"
-      ></Image>
-</Container>
-</div>
+
+<Card className="text-white" style={{ width: '90rem', height: "50rem"}}>
+  <Card.Img src= "Images/ImageAboveNew.jpg" alt="Card image"/>
+  <Card.ImgOverlay>
+    <Card.Title>Card title</Card.Title>
+    <Card.Text>
+      This is a wider card with supporting text below as a natural lead-in to
+      additional content. This content is a little bit longer.
+    </Card.Text>
+    <Card.Text>Last updated 3 mins ago</Card.Text>
+  </Card.ImgOverlay>
+</Card>
+<br></br>
 
 {/* Second Div with William's quote 
 Note: need to center text */}
 
-     <Container>
-     <Row>
-    <p>
-    <Col></Col>
-    </p>
-    <br></br>
-    <p>
-    <Col></Col>
-    </p>
-    <br></br>
-    </Row>
-        <Row className="justify-content-center, .font"> 
-          <p>
-         "Winemakers are artisans who deserve exceptional vessels that enable
+      <Card className="card">
+      <Card.Body className="justify-content-center" style={{ backgroundColor: '#EEECE6'}}>
+      <Card.Title class="text-body">"Winemakers are artisans who deserve exceptional vessels that enable
           their wines to express their full potential without any distraction"
-          </p>
-        </Row>
-
-      <Row className="justify-content-center">
-        <p>
+          <p>
         William Souter, CO-Founder
         </p>
-      </Row>
-      </Container>
+        </Card.Title>
+      <br></br>
+      </Card.Body>
+</Card>
 
 {/* Third part with concrete back 
 Note: need text on image */}
 
-<div>
-  <Container fluid >
-      <Image src= "Images/ConcreteBackNew.jpg" rounded
-      width= "100%"
-      ></Image>
-      <div class=""></div>
-  </Container>
-</div>
 
-<Jumbotron fluid style={{backgroundColor: '#F1EDE2'}}>
-  <Container>
-    <h1 className="justify-content-center">Fluid jumbotron</h1>
-    <p>
-      This is a modified jumbotron that occupies the entire horizontal space of
-      its parent.
-    </p>
-  </Container>
-</Jumbotron>
+<Card className="text-white" style={{ width: '90rem', height: "30rem"}}>
+  <Card.Img src= "Images/ConcreteBackNew.jpg" alt="Card image"/>
+  <Card.ImgOverlay>
+    <Card.Title>Card title</Card.Title>
+    <Card.Text>
+      This is a wider card with supporting text below as a natural lead-in to
+      additional content. This content is a little bit longer.
+    </Card.Text>
+    <Card.Text>Last updated 3 mins ago</Card.Text>
+  </Card.ImgOverlay>
+</Card>
 
 
 {/* Last part with image left and text right 
 Note: Button needs a dark border */}
 
 <div>
-<Card style={{ backgroundColor: '#F1EDE2'}}>
+<Card style={{ backgroundColor: '#EEECE6'}}>
     <Row>
       <Col>
       <br></br>
@@ -142,8 +127,8 @@ Note: Button needs a dark border */}
 </div>
 
 
-<Card>
-      <Card.Body className="justify-content-xl-center" style={{ backgroundColor: '#F1EDE2'}}>
+<Card className="card">
+      <Card.Body className="justify-content-xl-center" style={{ backgroundColor: '#EEECE6'}}>
       <Card.Title class="text-body">Customise your tank's finish, features, fittings and more!</Card.Title>
       <br></br>
       <Button variant="outline-secondary">Enquire Now</Button> 
@@ -153,13 +138,13 @@ Note: Button needs a dark border */}
 
 <Card>
   <Card.Body style={{ backgroundColor: '#061306'}} >
-    <Card.Title class="text-white">CONTACT US</Card.Title>
+    <Card.Title class="text-white"><b>CONTACT US</b></Card.Title>
     <br></br>
-    <Card.Text class="text-white">
-      sales@cellierwinetanks.com
+    <Card.Link class="text-white" href="contactpage">sales@cellierwinetanks.com</Card.Link>
     <br></br>
-      FACEBOOK | INSTAGRAM | LINKEDIN
-    </Card.Text>
+    <Card.Link class="text-white" href="contactpage"> FACEBOOK |</Card.Link>
+    <Card.Link class="text-white" href="contactpage"> INSTAGRAM |</Card.Link>
+    <Card.Link class="text-white" href="contactpage"> LINKEDIN</Card.Link>
     <Card.Text class="text-white">
       __________________________________________________________________________________________________
     </Card.Text>
